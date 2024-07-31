279abba8d3db033846a53126a6057718edfeb786
Shivansh-22866 <shivanshp0418@gmail.com>
2024-07-31 22:42:34 +0530

    assets added

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -11,34 +11,31 @@ class MyAppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-			title: 'Firebase Auth',
-			theme: ThemeData(
-				colorScheme: const ColorScheme.light(
-          surface: Colors.white,
-          onSurface: Colors.black,
-          primary: Color.fromRGBO(206, 147, 216, 1),
-          onPrimary: Colors.black,
-          secondary: Color.fromRGBO(244, 143, 177, 1),
-          onSecondary: Colors.white,
-					tertiary: Color.fromRGBO(255, 204, 128, 1),
-          error: Colors.red,
-					outline: Color(0xFF424242)
+        title: 'Firebase Auth',
+        theme: ThemeData(
+          colorScheme: const ColorScheme.light(
+              surface: Colors.white,
+              onSurface: Colors.black,
+              primary: Color.fromRGBO(248, 22, 79, 1),
+              onPrimary: Colors.black,
+              secondary: Color.fromRGBO(55, 100, 91, 1),
+              onSecondary: Colors.white,
+              tertiary: Color.fromRGBO(255, 204, 128, 1),
+              error: Colors.red,
+              outline: Color(0xFF424242)),
         ),
-			),
-			home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-				builder: (context, state) {
-					if(state.status == AuthenticationStatus.authenticated) {
-						return BlocProvider(
-							create: (context) => SignInBloc(
-								userRepository: context.read<AuthenticationBloc>().userRepository
-							),
-							child: const HomeScreen(),
-						);
-					} else {
-						return const WelcomeScreen();
-					}
-				}
-			)
-		);
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+            builder: (context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return BlocProvider(
+              create: (context) => SignInBloc(
+                  userRepository:
+                      context.read<AuthenticationBloc>().userRepository),
+              child: const HomeScreen(),
+            );
+          } else {
+            return const WelcomeScreen();
+          }
+        }));
   }
 }
